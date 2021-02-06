5c5191677a2c81e4573bef7562b7d3c4e7d34be7
Somer Jackson <somer.jackson@gmail.com>
2021-02-05 21:17:48 -0500

    Add database schema and seed file

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whooper', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Quarter Pounder', true);
