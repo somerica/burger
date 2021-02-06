36fe19c706262e8ce7b727c4c317c026609ceb41
Somer Jackson <somer.jackson@gmail.com>
2021-02-05 21:27:52 -0500

    Create controller and models for burger app

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+var burgers = require('../models/burger.js');
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router; 
